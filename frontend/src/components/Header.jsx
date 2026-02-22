e0dddcfe2ab6f92533b1f63461541e325ab7d8d1
Tapan <tapansgupta777@gmail.com>
2026-02-23 00:28:39 +0530

    a

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -47,19 +47,19 @@ const Header = () => {
               <Cpu className="text-[#00d4ff]" size={22} />
             </div>
 
-          <span className="italic font-['Times_New_Roman'] flex items-center gap-1 leading-none">
-            g<sub className="text-[0.7em] align-sub">m</sub>
-            <span className="mx-1">=</span>
+            <span className="italic font-['Times_New_Roman'] flex items-center gap-1 leading-none">
+              g<sub className="text-[0.7em] align-sub">m</sub>
+              <span className="mx-1">=</span>
 
-            <span className="flex flex-col items-center text-base leading-[1.05]">
-              <span className="border-b border-current px-1 pb-[2px] -mb-[2px]">
-                ∂I<sub className="text-[0.7em] align-sub">DS</sub>
-              </span>
-              <span className="px-1 pt-[2px]">
-                ∂V<sub className="text-[0.7em] align-sub">GS</sub>
+              <span className="flex flex-col items-center text-base leading-[1.05]">
+                <span className="border-b border-current px-1 pb-[2px] -mb-[2px]">
+                  ∂I<sub className="text-[0.7em] align-sub">DS</sub>
+                </span>
+                <span className="px-1 pt-[2px]">
+                  ∂V<sub className="text-[0.7em] align-sub">GS</sub>
+                </span>
               </span>
             </span>
-          </span>
 
           {/* Desktop Navigation */}
           <nav className="hidden lg:flex items-center gap-8">
