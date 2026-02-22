d53ebe6cf23678840932a5f58d95789dce6a7306
Tapan <tapansgupta777@gmail.com>
2026-02-22 23:01:36 +0530

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
         {/* Main Portfolio Route */}
         <Route
