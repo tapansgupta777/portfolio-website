ea288d6dac0906cc426f1ccd423b3559a4549da1
Tapan <tapansgupta777@gmail.com>
2026-02-24 23:17:14 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
 
                 {/* Main word */}
                 <span className="font-semibold">
-                  Analog
+                  analog
                 </span>
 
               </span>
